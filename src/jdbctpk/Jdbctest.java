1678d9f7710ce30d54e430a9e3b750635fccd0ee
Mahanteshkuar <kumarmanthi@gmail.com>
2019-04-24 12:53:41 +0530

    javaproject

---
+++ src/jdbctpk/Jdbctest.java
@@ -0,0 +1,19 @@
+package jdbctpk;
+import java.sql.*;
+public class Jdbctest 
+{
+	public static void main(String[] args) throws SQLException 
+	{
+		Connection mycon=null;
+		Statement myst=null;
+		ResultSet myrst=null;
+		mycon=DriverManager.getConnection("jdbc:mysql://localhost:3306//demo", "studen", "student@123");
+		System.out.println("data base connected");
+		myst=mycon.createStatement();
+		myrst=myst.executeQuery("select * from employees");
+		while(myrst.next())
+		{
+			System.out.println(myrst.getString("first_name")+"  ,  "+myrst.getString("last_name"));
+		}
+	}
+}
